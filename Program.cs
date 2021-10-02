2e6a135623c57dac65894612a816c0b7df589f7d
jrod-disco <jrod.disco@gmail.com>
2021-10-01 23:24:25 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gamejam_ld49
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
